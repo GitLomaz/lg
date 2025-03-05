7b1c2021c9b724c3e25bbb06ec93193187b8c317
Ian Lomas <you@example.com>
2025-03-05 13:24:12 -0500

    intro to sessions

--- lg-api/src/main.ts
+++ lg-api/src/main.ts
@@ -1,14 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
 
   app.enableCors({
     origin: 'http://localhost:3001', // Allow requests from frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     // credentials: true, // If using cookies/auth
   });
+
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET || 'supersecretkey', // Change this in production
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production', // Only secure in production
+        maxAge: 1000 * 60 * 60 * 24, // 1 day
+      },
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
