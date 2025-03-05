e393d23d3ff3fade1f855c4b4e35d25c9f38b623
Ian Lomas <you@example.com>
2025-03-05 14:37:18 -0500

    intro to sessions

--- lg-api/src/app.module.ts
+++ lg-api/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { GamesModule } from './games/games.module';
+import { AuthModule } from './auth/auth.module';
+import { MailModule } from './mail/mail.module';
 
 @Module({
-  imports: [DatabaseModule, GamesModule],
+  imports: [DatabaseModule, GamesModule, AuthModule, MailModule],
   controllers: [AppController],
   providers: [AppService],
 })
