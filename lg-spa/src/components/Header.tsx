6cc998f2b8516a49fc9e257cc4f794398b731876
Ian Lomas <you@example.com>
2025-03-19 21:29:57 -0400

    logo

--- lg-spa/src/components/Header.tsx
+++ lg-spa/src/components/Header.tsx
@@ -5,12 +5,18 @@ import { useUserState } from '../contexts/useUserState';
 const Header: React.FC = () => {
   const { user, setUser } = useUserState()
 
-
   return (
-    <div className='app-header'>
-      <a href='/'>
-        <h1>Lomaz Games</h1>
-      </a>
+    // Add 3 flexboxes here
+    <div className='app-header flex-row'>
+      <div id='auth' className='flex-item-1'>
+        <div id='logo'></div>
+      </div>
+      <div id='header-home' className='flex-item-1'>
+        <a href='/'>
+          <h1>Lomaz Games</h1>
+        </a>
+      </div>
+      <div id='auth' className='flex-item-1'>Log In</div>
     </div>
   );
 };
