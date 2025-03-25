38ef726d05680e37515ca4a3ad20bfec29bada5f
Ian Lomas <you@example.com>
2025-03-25 17:55:44 -0400

    basic auth sessions WIP

--- lg-api/src/auth/auth.controller.ts
+++ lg-api/src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Body, Query, Get, UseGuards, Session } from '@nestjs/common';
+import { Controller, Post, Body, Query, Get, UseGuards, Session, Req } from '@nestjs/common';
+import { Request } from 'express';
 import { AuthService } from './auth.service';
 import { UserService } from '../user/user.service'
-import { AuthGuard } from '@nestjs/passport';
+import { AuthenticatedGuard, LocalAuthGuard } from './LocalGuard';
 
 @Controller('auth')
 export class AuthController {
@@ -20,17 +21,22 @@ export class AuthController {
     return this.authService.verifyEmail(token);
   }
 
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   @Post('login')
   async getUserByEmailOrUsername(@Body() body) {
     let user = await this.authService.validateUser(body.username, body.password)
-    console.log(user)
     return user
-    // return this.authService.verifyEmail(token);
   }
 
   @Get('')
   async getAuthSession(@Session() session: Record<string, any>) {
-    console.log(session.id)
+    session.authenticated = true
+    return session
+  }
+
+  @UseGuards(AuthenticatedGuard)
+  @Get('status')
+  async getAuthStatus(@Req() req: Request) {
+    return req.user
   }
 }
--- lg-api/src/auth/auth.module.ts
+++ lg-api/src/auth/auth.module.ts
@@ -3,13 +3,24 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { MailModule } from '../mail/mail.module';
 import { UserModule } from '../user/user.module';
-import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './LocalStrategy';
+import { SessionSerializer } from './SessionSerializer';
+import { DatabaseModule } from '../database/database.module';
+import { PassportModule } from '@nestjs/passport';
+import { DatabaseService } from 'src/database/database.service';
 
 @Module({
-  imports: [MailModule, UserModule, PassportModule],
+  imports: [MailModule, UserModule, PassportModule, DatabaseModule],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy,
+    {
+      provide: SessionSerializer,
+      useFactory: (databaseService: DatabaseService) => {
+        return new SessionSerializer(databaseService);
+      },
+      inject: [DatabaseService], // Injecting the DatabaseService
+    },
+  ],
   exports: [AuthService],
 })
 export class AuthModule {}
