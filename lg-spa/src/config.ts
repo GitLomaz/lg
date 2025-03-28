caab6e4a69b6d8cbd02aa5dbfc5e5af836f1b1a8
Ian Lomas <you@example.com>
2025-03-27 21:28:00 -0400

    blargh

--- lg-spa/src/config.ts
+++ lg-spa/src/config.ts
@@ -1,3 +1,3 @@
-const REACT_APP_API_URL = process.env.REACT_APP_API_URL || "http://localhost:3000";
+const REACT_APP_API_URL = process.env.REACT_APP_API_URL || "https://lomazgames.com:3000";
 
 export default REACT_APP_API_URL;
