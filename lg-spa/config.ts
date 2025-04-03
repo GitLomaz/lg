0fcc121aeb56cec006d3f4db7dd9ab5717456da7
Ian Lomas <you@example.com>
2025-04-03 10:47:30 -0400

    Auto-update: Thu, Apr  3, 2025 10:47:29 AM

--- lg-spa/config.ts
+++ lg-spa/config.ts
@@ -1,1 +1,1 @@
-const REACT_APP_API_URL = process.env.REACT_APP_API_URL || "https://lomazgames.com:3000"; export default REACT_APP_API_URL;
+const REACT_APP_API_URL = "https://lomazgames.com:3000"; export default REACT_APP_API_URL;
