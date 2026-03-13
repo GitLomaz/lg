e8a5663dd9afaddf39e56ce1dd93c3ec14e19b81
Ian Lomas <you@example.com>
2026-03-13 10:07:18 -0400

    docker

--- start.sh
+++ start.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 set -e
 
-# Construct database URL from individual variables
-export API_DATABASE_URL="mysql://${DB_USER}:${DB_PASS}@${DB_HOST}:3306/${DB_NAME}?schema=public"
+export API_DATABASE_URL="mysql://${DB_USER}:${DB_PASS}@${DB_HOST}:3306/${DB_NAME}"
 
-# Start NestJS API in background
 cd /app/api
 node dist/main &
 
-# Start nginx in foreground
 nginx -g 'daemon off;'
