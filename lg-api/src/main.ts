1e47e00c8ee76ac4d7cdfa70bd24a5617963191e
Ian Lomas <you@example.com>
2026-03-13 10:23:38 -0400

    docker

--- lg-api/src/main.ts
+++ lg-api/src/main.ts
@@ -2,10 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
 import * as cookieParser from 'cookie-parser';
-import * as session from "express-session";
-import * as passport from "passport";
+import * as session from 'express-session';
+import * as passport from 'passport';
 
 config();
+
 async function bootstrap() {
   let origin = 'http://localhost:3210';
   if (process.env.API_LOCAL !== 'true') {
@@ -17,7 +18,7 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    origin: origin,
+    origin,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
@@ -28,13 +29,17 @@ async function bootstrap() {
       secret: process.env.API_SESSION_SECRET,
       resave: false,
       saveUninitialized: false,
-      cookie: { secure: process.env.API_LOCAL !== 'true', maxAge: 1000 * 60 * 60 },
-    })
+      cookie: {
+        secure: process.env.API_LOCAL !== 'true',
+        maxAge: 1000 * 60 * 60,
+      },
+    }),
   );
 
   app.use(passport.initialize());
   app.use(passport.session());
 
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  await app.listen(3000, '0.0.0.0');
 }
+
 bootstrap();
